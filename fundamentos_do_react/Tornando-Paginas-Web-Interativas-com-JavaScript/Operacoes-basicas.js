751280e83c5bc53ac6703c724cb78223bd1a606f
Jaelson Freitas <jaelsonfreitas@yahoo.com.br>
2024-01-01 19:10:20 -0300

    Aula laços de repetição.

--- fundamentos_do_react/Tornando-Paginas-Web-Interativas-com-JavaScript/Operacoes-basicas.js
+++ fundamentos_do_react/Tornando-Paginas-Web-Interativas-com-JavaScript/Operacoes-basicas.js
@@ -34,3 +34,72 @@ switch (nota) {
 
 const note = 2
 note > 5 ? console.log('Aprovado') : console.log('reprovado');
+
+// Aula 03 - Tipos de Variaveis.
+
+//Em JavaScript, variáveis são utilizadas para armazenar e representar dados. Elas são como "contêineres" que você pode usar para armazenar valores, e esses valores podem ser de diversos tipos, como números, strings, booleanos, objetos, funções, entre outros.
+
+//Em JavaScript, você pode declarar variáveis usando as palavras-chave var, let ou const. A diferença entre elas está relacionada ao escopo e à mutabilidade da variável:
+
+//var: Declara uma variável com escopo de função. Antigamente, var era a única forma de declarar variáveis em JavaScript, mas ela possui algumas características que podem causar confusão, como hoisting (içamento) e escopo global.
+
+var x = 10;
+
+//let: Introduz uma variável com escopo de bloco. Variáveis declaradas com let podem ser reatribuídas.
+
+let y = "Olá, mundo!";
+
+//const: Declara uma variável com escopo de bloco, assim como let, mas a diferença é que uma vez que você atribui um valor a uma constante, não pode reatribuir um novo valor a ela.
+
+const PI = 3.14;
+
+
+
+
+
+
+
+//Tipos de Variáveis
+// Boolean = true e fale
+//null = Nulo
+//undefined = Não definido
+//Number 42 ou 3.14159
+//String = "Jaelson"
+
+// Declarando variáveis
+let nome = "João";
+var age = 25;
+const altura = 1.75;
+
+
+// Utilizando variáveis
+console.log("Nome:", nome);
+console.log("Idade:", age);
+console.log("Altura:", altura);
+
+// Modificando o valor de uma variável
+age = 26;
+console.log("Nova idade:", age);
+
+//Laços de Repetição
+console.log('Tabuada do 5, utilizando laço de repetição.');
+for (let index =1; index <= 10; index ++){
+    console.log(`${index} x 5 = ${index * 5}`);
+}
+
+
+console.log('===========================');
+let tab = 1;
+while(tab < 11) {
+    console.log(`${tab} x 5 = ${tab * 5}`);
+    tab++;
+}
+
+console.log('*******************');
+
+// forEach normalmente utilizamos par se trabalhar com arrays.
+
+const valores = [1,2,3,4,5,6,7,8,9,10];
+valores.forEach((value) => {
+    console.log(`${value} x 5 = ${value * 5}`);
+});
