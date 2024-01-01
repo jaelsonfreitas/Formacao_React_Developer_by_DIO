fe747df5a1383e9c327acb405819f2011571f334
Jaelson Freitas <jaelsonfreitas@yahoo.com.br>
2024-01-01 14:47:40 -0300

    Aula 1

--- fundamentos_do_react/Tornando-Paginas-Web-Interativas-com-JavaScript/Operacoes-basicas.js
+++ fundamentos_do_react/Tornando-Paginas-Web-Interativas-com-JavaScript/Operacoes-basicas.js
@@ -1,1 +1,8 @@
-// Inicio teste
+// Aula 01 - Operações básicas.
+
+let valor1 = 4
+let valor2 = 4
+console.log('A soma é:', valor1 + valor2);
+console.log('A subtração é:', valor1 - valor2);
+console.log('A multiplicação é:', valor1 * valor2);
+console.log('A divisão é:', valor1 / valor2);
