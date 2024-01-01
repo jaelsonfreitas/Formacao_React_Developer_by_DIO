c4b5d74ee1ab4eff8c8c1adf550e1f5f5f822b23
Jaelson Freitas <jaelsonfreitas@yahoo.com.br>
2024-01-01 15:16:21 -0300

    Condicionais Aula 02

--- fundamentos_do_react/Tornando-Paginas-Web-Interativas-com-JavaScript/Operacoes-basicas.js
+++ fundamentos_do_react/Tornando-Paginas-Web-Interativas-com-JavaScript/Operacoes-basicas.js
@@ -6,3 +6,31 @@ console.log('A soma é:', valor1 + valor2);
 console.log('A subtração é:', valor1 - valor2);
 console.log('A multiplicação é:', valor1 * valor2);
 console.log('A divisão é:', valor1 / valor2);
+
+// Aula 02 - Condicionais
+// As estruturas condicionais estão ligadas a tomadas de decisões de um algoritimo. Ao utilizarmos as expressões que retornam verdadeiro ou falso, o algoritimo executa o bloco de comandos relativos a este resultado.
+
+const idade = 15;
+
+if(idade > 18){
+    console.log('Maior que 18');
+}else{
+    console.log('Menor que 18.');
+}
+
+const nota = 4;
+
+switch (nota) {
+    case nota < 5:
+        console.log("Abaixo da média");
+        break;
+        case nota  > 5:
+            console.log("Dentro da média");
+            break;    
+            default:
+        console.log('Acima da média.');
+        break;
+}
+
+const note = 2
+note > 5 ? console.log('Aprovado') : console.log('reprovado');
