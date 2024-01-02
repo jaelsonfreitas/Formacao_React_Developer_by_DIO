2faad0b8d8a9a28190878c5aec7611c70f526b25
Jaelson Freitas <jaelsonfreitas@yahoo.com.br>
2024-01-02 14:06:13 -0300

    Aulas sobre DOM.

---
+++ fundamentos_do_react/Entendendo-a-Dom/script.js
@@ -0,0 +1,13 @@
+//Dom (Document Object Model)
+// Dom é a representação de dados dos objetos que compõem a estrutura e o conteúdo de uma documento na Web. O Document Object Model é uma interface de programação para os documentos HTML e XML.
+//Representa a página de forma que os programas possam alterar a estrutura do documento, alterar os estilo e conteúdo.
+//Dom representa o documento com nós e objetos. 
+
+// Operações basicas
+
+// 1 - getElementsByTagName
+// 2 - getElementsByClassName
+// 3 - getElementsById
+// 4 - querySelector
+
+window.document.querySelectorAll('title')
