0a7d1b0d4a6575c26221235c6827b4191d1b3317
Jaelson Freitas <jaelsonfreitas@yahoo.com.br>
2024-01-07 23:12:57 -0300

    Componentes.

--- fundamentos_do_react/Primeiros_Passos_na_Biblioteca_React/dio/src/App.js
+++ fundamentos_do_react/Primeiros_Passos_na_Biblioteca_React/dio/src/App.js
@@ -1,9 +1,12 @@
+import Button from "../src/components/button";
+
 function App() {
-  return (
-    <div className="App">
-      Olá React
+  return ( 
+  <div className="App">
+    <h1>Olá React</h1>
+    <Button title="Entrar" />
     </div>
-  );
+    );
 }
 
 export default App;
